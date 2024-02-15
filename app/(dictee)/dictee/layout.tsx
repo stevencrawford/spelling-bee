d4e95e068078785e61b4a4c540923b43c1812e5f
Steven Crawford <steven.crawford.uk@gmail.com>
2024-02-15 15:24:03 +0100

    refactor: simplify layout

--- app/(dictee)/dictee/layout.tsx
+++ app/(dictee)/dictee/layout.tsx
@@ -1,4 +1,3 @@
-import { DicteeShell } from '@/components/dictee-shell';
 import { buttonVariants } from '@/components/ui/button';
 import Link from 'next/link';
 import { Icons } from '@/components/icons';
@@ -10,23 +9,18 @@ interface DicteeLayoutProps {
 
 export default function DicteeLayout({ children }: DicteeLayoutProps) {
   return (
-    <div className="container grid gap-10 py-8">
-      <DicteeShell>
-        <main className="flex w-full flex-1 flex-col overflow-hidden">
-          {children}
-        </main>
-        <div className="fixed bottom-5 right-5 grid w-full justify-items-end">
-          <Link
-            href="/dictee/new"
-            className={cn(
-              buttonVariants({ variant: 'default' }),
-              'rounded-full',
-            )}
-          >
-            <Icons.add className="h-6 w-6" />
-          </Link>
-        </div>
-      </DicteeShell>
+    <div className="container grid items-start gap-10 py-8">
+      <main className="flex w-full flex-1 flex-col overflow-hidden">
+        {children}
+      </main>
+      <div className="fixed bottom-5 right-5 grid w-full justify-items-end">
+        <Link
+          href="/dictee/new"
+          className={cn(buttonVariants({ variant: 'default' }), 'rounded-full')}
+        >
+          <Icons.add className="h-6 w-6" />
+        </Link>
+      </div>
     </div>
   );
 }
