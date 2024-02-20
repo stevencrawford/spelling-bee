d11cf3e76551ba5d38dfe38c878904f8fc3515d6
Steven Crawford <steven.crawford.uk@gmail.com>
2024-02-20 15:04:31 +0100

    fix: add noStore directive to prisma query

--- app/(spelling)/spelling/page.tsx
+++ app/(spelling)/spelling/page.tsx
@@ -1,7 +1,10 @@
 import {SpellingListItem} from '@/components/spelling-list-item';
 import {db} from '@/lib/db';
+import { unstable_noStore as noStore } from 'next/cache';
 
 async function getSpelling() {
+  noStore();
+
   return await db.spelling.findMany({
     select: {
       id: true,
